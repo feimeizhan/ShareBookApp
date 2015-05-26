6bd11845e6dc29ebd49e480eeb3fad4d3339e5e5
feimeizhan <feimei.zhan@live.com>
2015-05-26 22:35:44 +0800

    添加了全局application

---
+++ app/src/main/java/com/justdoit/sharebook/util/MyApp.java
@@ -0,0 +1,10 @@
+package com.justdoit.sharebook.util;
+
+import android.app.Application;
+
+/**
+ * 1.存储application全局变量，比如，cookiestore
+ * Created by feimei.zhan on 2015/5/26.
+ */
+public class MyApp extends Application {
+}
